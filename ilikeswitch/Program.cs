1abcbf6f40e8d37286eebebd6b5680755cb5e257
소원 박 <s2018s29@e-mirim.hs.kr>
2020-05-06 19:59:59 +0900

    switch 조건문으로 변경

--- ilikeswitch/Program.cs
+++ ilikeswitch/Program.cs
@@ -10,6 +10,32 @@ namespace ilikeswitch
     {
         static void Main(string[] args)
         {
+            Console.Write("학년을 입력하세요 : ");
+            int level = int.Parse(Console.ReadLine());
+            Console.Write("수강해야 하는 전공 학점 : ");
+            switch (level)
+            {
+                case 1:
+                    Console.Write("12");
+                    break;
+
+                case 2:
+                    Console.Write("18");
+                    break;
+
+                case 3:
+                    Console.Write("10");
+                    break;
+
+                case 4:
+                    Console.Write("18");
+                    break;
+
+                default:
+                    Console.WriteLine("잘못 입력하셨습니다");
+                    break;
+            }
+            Console.WriteLine("학점");
         }
     }
 }
